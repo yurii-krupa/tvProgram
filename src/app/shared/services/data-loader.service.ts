71a292583be9b0d71d81234ac0971f1da0887d4a
Yurii Krupa <yurii.krupa.dev@gmail.com>
2020-04-06 13:52:43 +0300

    [~] - trying to deploy

--- src/app/shared/services/data-loader.service.ts
+++ src/app/shared/services/data-loader.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLoaderService {
 
-  private apiURL = 'http://localhost:3000';
+  private apiURL = environment.API_URL || 'http://localhost:3000';
 
   constructor(
     private http: HttpClient
